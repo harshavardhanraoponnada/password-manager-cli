3cd930f05fda5b2c43616c64ed3487a8ff7ce16a
Harsha Vardhan Rao Ponnada <harshavardhanraoponnada@gmail.com>
2025-12-15 19:39:30 +0530

    Improve authentication and add search functionality

--- password_manager/manager.py
+++ password_manager/manager.py
@@ -3,13 +3,26 @@ from password_manager.utils import generate_key, encrypt_data, decrypt_data
 
 MASTER_KEY = None
 
+from cryptography.fernet import InvalidToken
 
 def authenticate():
     global MASTER_KEY
     master_password = input("Enter master password: ").strip()
 
     salt = get_salt()
-    MASTER_KEY = generate_key(master_password, salt)
+    key = generate_key(master_password, salt)
+
+    vault = load_vault()
+    if vault:
+        try:
+            sample = next(iter(vault.values()))
+            decrypt_data(sample["password"], key)
+        except InvalidToken:
+            print("❌ Incorrect master password.")
+            exit(1)
+
+    MASTER_KEY = key
+
 
 
 def add_password():
@@ -43,4 +56,18 @@ def view_passwords():
     print("\nStored credentials:")
     for service, creds in vault.items():
         decrypted_pwd = decrypt_data(creds["password"], MASTER_KEY)
-        print(f"- {service} | {creds['username']} | {decrypted_pwd}")
+        masked = "*" * len(decrypted_pwd)
+        print(f"- {service} | {creds['username']} | {masked}")
+
+def search_password():
+    service = input("Enter service name to search: ").strip()
+    vault = load_vault()
+
+    if service not in vault:
+        print("Service not found.")
+        return
+
+    creds = vault[service]
+    decrypted_pwd = decrypt_data(creds["password"], MASTER_KEY)
+    masked = "*" * len(decrypted_pwd)
+    print(f"{service} | {creds['username']} | {masked}")
